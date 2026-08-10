757f04c3853d66d9be9a64cba66c97348b75f75b
Shaik Sajida <anwarjhanshaik@gmail.com>
2026-08-10 22:57:06 +0530

    Add capacity and size methods to Stack class
    Refactor stack implementation to include capacity and size methods.

--- Stack/stack_linked_list.py
+++ Stack/stack_linked_list.py
@@ -1,4 +1,6 @@
-class EmptyStackError(Exception):
+class EmptyStackError(IndexError):
+  pass
+class FullStackError(OverflowError):
   pass
 class Node:
   def __init__(self, data, next=None):
@@ -6,21 +8,32 @@ class Node:
     self.next = next
 
 class Stack:
-  def __init__(self):
+  def __init__(self, capacity=None):
     self.start = None
+    self.capacity = capacity
     self._size = 0
+
+  def __len__(self):
+    return self.size
 
   def is_empty(self):
     return self.start is None
 
+  def is_full(self):
+    if self.capacity is None:
+      return False
+    return len(self) >= self.capacity
+
   def push(self, item):
+    if self.is_full():
+      raise FullStackError("'Stack' is full; cannot push an item")
     node = Node(item, self.start)
     self.start = node
     self._size += 1
 
   def pop(self):
     if self.is_empty():
-      raise EmptyStackError("'Stack' is empty cannot pop an item")
+      raise EmptyStackError("'Stack' is empty; cannot pop an item")
     deleted_item = self.start.data
     self.start = self.start.next
     self._size -= 1
@@ -31,6 +44,5 @@ class Stack:
       raise EmptyStackError("'Stack' is empty")
     return self.start.data
 
-  def size(self):
-    return self._size
+  
     
