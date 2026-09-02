797743b4f8aa775a5eff4cc13a38863ccaa8e393
Shaik Sajida <anwarjhanshaik@gmail.com>
2026-09-02 22:43:46 +0530

    Fix typo in Deque error class names

--- Deque/deque_dll.py
+++ Deque/deque_dll.py
@@ -1,6 +1,6 @@
-class DequeUnderFlowError(IndexError):
+class DequeUnderflowError(IndexError):
   pass
-class DequeOverFlowError(OverflowError):
+class DequeOverflowError(OverflowError):
   pass
 
 class Node:
@@ -26,8 +26,8 @@ class Deque:
   def __str__(self):
     if self.is_empty():
       return "Deque is empty"
-    result = [str(node_data) for node_data in self]
-    return f"=== DEQUE ITEMS ===\n[FRONT] {' -> '.join(result)} [REAR]"
+    deque_items = [str(node_data) for node_data in self]
+    return f"=== DEQUE ITEMS ===\n[FRONT] {' -> '.join(deque_items)} [REAR]"
 
   def __repr__(self):
     return f"Deque(size={len(self)})"
@@ -42,7 +42,7 @@ class Deque:
 
   def insert_at_front(self, item):
     if self.is_full():
-      raise DequeOverFlowError("Cannot insert an item; 'Deque' is full")
+      raise DequeOverflowError("Cannot insert an item; 'Deque' is full")
     node = Node(item, next=self.front)
     if self.is_empty():
       self.rear = node
@@ -53,7 +53,7 @@ class Deque:
 
   def insert_at_rear(self, item):
     if self.is_full():
-      raise DequeOverFlowError("Cannot insert an item; 'Deque' is full")
+      raise DequeOverflowError("Cannot insert an item; 'Deque' is full")
     node = Node(item, prev=self.rear)
     if self.is_empty():
       self.front = node
@@ -64,36 +64,36 @@ class Deque:
 
   def delete_front(self):
     if self.is_empty():
-      raise DequeUnderFlowError("Cannot delete an item; 'Deque' is empty")
-    del_node = self.front.data
+      raise DequeUnderflowError("Cannot delete an item; 'Deque' is empty")
+    del_node_data = self.front.data
     if self.front == self.rear:
       self.front = self.rear = None
     else:
       self.front = self.front.next
       self.front.prev = None
     self.size -= 1
-    return del_node
+    return del_node_data
 
   def delete_rear(self):
     if self.is_empty():
-      raise DequeUnderFlowError("Cannot delete an item; 'Deque' is empty")
-    del_node = self.rear.data
+      raise DequeUnderflowError("Cannot delete an item; 'Deque' is empty")
+    del_node_data = self.rear.data
     if self.front == self.rear:
       self.front = self.rear = None
     else:
       self.rear.prev.next = None
       self.rear = self.rear.prev
     self.size -= 1
-    return del_node
+    return del_node_data
 
   def get_front(self):
     if self.is_empty():
-      raise DequeUnderFlowError("Cannot peek front; 'Deque' is empty")
+      raise DequeUnderflowError("Cannot peek front; 'Deque' is empty")
     return self.front.data
 
   def get_rear(self):
     if self.is_empty():
-      raise DequeUnderFlowError("Cannot peek rear; 'Deque' is empty")
+      raise DequeUnderflowError("Cannot peek rear; 'Deque' is empty")
     return self.rear.data
 
   def clear(self):
